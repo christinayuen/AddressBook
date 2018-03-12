79d4810df65fba541f67d0400de97319418a74e9
chris_yuen <chris_yuen@outlook.com>
2018-03-12 21:15:26 +1100

    Add project files.

---
+++ AddressBook/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace AddressBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAddContact = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Contacts";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(3, 18);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(230, 405);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.ContextMenuStripChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnSave);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.btnAddContact);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(254, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(534, 423);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Information";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Email";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(395, 375);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(119, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(20, 375);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(130, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAddContact
+            // 
+            this.btnAddContact.Location = new System.Drawing.Point(208, 375);
+            this.btnAddContact.Name = "btnAddContact";
+            this.btnAddContact.Size = new System.Drawing.Size(132, 23);
+            this.btnAddContact.TabIndex = 8;
+            this.btnAddContact.Text = "Add Contact";
+            this.btnAddContact.UseVisualStyleBackColor = true;
+            this.btnAddContact.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(126, 194);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(402, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Last Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(402, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(402, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mobile";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(402, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Contact List";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddContact;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AddressBook/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+//created by Christina Yuen
+
+namespace AddressBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Contacts> contact = new List<Contacts>();
+        
+        class Contacts
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Mobile { get; set; }
+            public string Email { get; set; }
+        }
+        
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* This function is for the add contact button. I also wanted the contact to display the first and last name
+             * on the listview when contact is added.
+             * It will also clear all inputted data when contact is added.*/
+
+            
+            Contacts a = new Contacts();
+            listView1.Sorting = SortOrder.Ascending;
+
+            //contact.OrderBy(x => x).ToList();
+           
+
+            a.FirstName = textBox1.Text;
+            a.LastName = textBox2.Text;
+            a.Email = textBox3.Text;
+            a.Mobile = textBox4.Text;
+            contact.Add(a);
+            listView1.Items.Add(a.FirstName + " " + a.LastName);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear(); 
+            textBox4.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /* This function is written for the delete button. I have written two lines of code because I want to
+             delete the name displayed in listview and also the element in the List<> field. I also wanted to clear all the 
+             information inputted in the textbox when deleteing the contact.*/
+
+            if (listView1.SelectedItems.Count > 0)
+            {
+                contact.RemoveAt(listView1.SelectedIndices[0]);
+                listView1.Items.RemoveAt(listView1.SelectedIndices[0]);
+            }
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /* This function is written for the save button. The first four lines is so that the save button will save
+             all the information entered. The last line of code is that if there is any editing to the first name, 
+             buy pressing save, the name displayed in the listview will also update and save.*/
+            if (listView1.SelectedItems.Count > 0)
+            {
+                contact[listView1.SelectedIndices[0]].FirstName = textBox1.Text;
+                contact[listView1.SelectedIndices[0]].LastName = textBox2.Text;
+                contact[listView1.SelectedIndices[0]].Mobile = textBox3.Text;
+                contact[listView1.SelectedIndices[0]].Email = textBox4.Text;
+                listView1.SelectedItems[0].Text = textBox1.Text;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* Here I have created a path to an xml file under the folder name app data.
+             * I have used an if statement so that if it does not exist, it will need to create one.
+             I have also created a root element for the path. The start root will begin with the name 'contacts*/
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            if (!Directory.Exists(path + "\\Address Book - Christina"))
+            {
+                Directory.CreateDirectory(path + "\\Address Book - Christina");
+            }
+
+            if (!File.Exists(path + "\\Address Book - Christina\\settings.xml"))
+            {
+                XmlTextWriter xWriter = new XmlTextWriter(path + "\\Address Book - Christina\\settings.xml", Encoding.UTF8);
+                xWriter.WriteStartElement("Contacts");
+                xWriter.WriteEndElement();
+                xWriter.Close();
+            }  
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /* This function is so that it updates when we select a different person on the listview */
+
+            if(listView1.SelectedItems.Count > 0)
+            {
+                textBox1.Text = contact[listView1.SelectedIndices[0]].FirstName;
+                textBox2.Text = contact[listView1.SelectedIndices[0]].LastName;
+                textBox3.Text = contact[listView1.SelectedIndices[0]].Mobile;
+                textBox4.Text = contact[listView1.SelectedIndices[0]].Email;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            /* This function is to access all the contacts from the contacts list and write it to the xml file */
+ 
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(path + "\\Address Book - Christina\\settings.xml");
+            XmlNode xNode = xDoc.SelectSingleNode("Contacts");
+            xNode.RemoveAll();
+
+            foreach(Contacts a in contact)
+            {
+                /*Create a new node for each and store in the xMain node */
+                XmlNode xMain = xDoc.CreateElement("Contact");
+                XmlNode xFirstName = xDoc.CreateElement("FirstName");
+                XmlNode xLastName = xDoc.CreateElement("LastName");
+                XmlNode xMobile = xDoc.CreateElement("Mobile");
+                XmlNode xEmail = xDoc.CreateElement("Email");
+
+                /* Store the information in the above nodes with the information entered in the form */
+                xFirstName.InnerText = a.FirstName;
+                xLastName.InnerText = a.LastName;
+                xMobile.InnerText = a.Mobile;
+                xEmail.InnerText = a.Email;
+
+                /* Store all the sub nodes into the xMain.*/
+                xMain.AppendChild(xFirstName);
+                xMain.AppendChild(xLastName);
+                xMain.AppendChild(xMobile);
+                xMain.AppendChild(xEmail);
+
+                /* Add the xMain node to the document of the Contacts node */
+                xDoc.DocumentElement.AppendChild(xMain);
+            }
+
+            /* Save the document to the provided path */
+            xDoc.Save(path + "\\Address Book - Christina\\settings.xml");
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /* This function is to use shortcuts to buttons, for accessibility */
+
+            if(e.Alt && e.KeyCode == Keys.D)
+            {
+                btnDelete.PerformClick();
+            }
+
+            if(e.Alt && e.KeyCode == Keys.A)
+            {
+                btnAddContact.PerformClick();
+            }
+
+            if(e.Alt && e.KeyCode == Keys.S)
+            {
+                btnSave.PerformClick();
+            }
+
+            if (e.KeyCode == Keys.Escape) 
+            {
+                Close();
+            }
+        }
+       
+    }
+}
